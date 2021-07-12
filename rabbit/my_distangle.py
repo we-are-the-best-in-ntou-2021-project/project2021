c97a7594c67420a9a864ce41f26b85e7a0d6de23
rabbit <yinming852@gmail.com>
2021-07-12 14:53:06 +0800

    Update my_distangle.py

--- rabbit/my_distangle.py
+++ rabbit/my_distangle.py
@@ -1,16 +1,18 @@
+from keras import backend as K
 from keras.models import Sequential,Model
-from keras.layers import Conv1D,MaxPooling1D,BatchNormalization,Dense,Activation,Flatten,Dropout
+from keras.layers import Conv1D,MaxPooling1D,BatchNormalization,Dense,Activation,Flatten,Dropout,GlobalAveragePooling1D
 from keras.layers import Concatenate,Input,Lambda
 from keras.optimizers import Adam
 import keras
 import numpy as np
 from numpy import load,save
-# import tensorflow.compat.v1 as tf #使用1.0版本的方法
-# tf.disable_v2_behavior() #禁用2.0版本的方法
+from keras.layers.advanced_activations import LeakyReLU
+import pandas as pd
+import preprocessData as pre
 import tensorflow as tf
-# import keras.backend as K
-# K.clear_session()
+import datetime
 
+# K.set_floatx('float64')
 import GPU_setting as G
 G.setting()
 
@@ -32,13 +34,23 @@ def get_data():
     data = np.vstack((data,a))
     labels = np.hstack((labels,b))
   
-  data_test = data[np.where(labels[0,:]>=5),:,:,:]
-  data = data[np.where(labels[0,:]<=2),:,:,:]
+  a = data
+  # print(pre.make_velocity(data).shape)
+  # data = np.concatenate((data,pre.make_offset(a)),axis=2)
+  data = np.concatenate((data,pre.make_velocity(a)),axis=2)
+  # data = np.concatenate((data,pre.make_accel(a)),axis=2)
+  # print(data.shape)
+
+  data_test = data[np.where(labels[0,:]==0),:,:,:]
+  data = data[np.where(labels[0,:]>=3),:,:,:]
   data_test = data_test.reshape(data_test.shape[1],data_test.shape[2],data_test.shape[3]*data_test.shape[4])
   data = data.reshape(data.shape[1],data.shape[2],data.shape[3]*data.shape[4])
-
-  labels_test = labels[:,np.where(labels[0,:]>=5)]
-  labels = labels[:,np.where(labels[0,:]<=2)]
+  print(data.shape)
+  
+  # labels = keras.utils.to_categorical(labels)
+  # print(labels)
+  labels_test = labels[:,np.where(labels[0,:]==0)]
+  labels = labels[:,np.where(labels[0,:]>=3)]
   labels_test = labels_test.reshape(labels_test.shape[0],labels_test.shape[2])
   labels = labels.reshape(labels.shape[0],labels.shape[2])
 
@@ -48,150 +60,198 @@ def get_data():
   labels[0] = labels[0][index]
   labels[1] = labels[1][index]
 
-  labels_v = labels[0]
-  labels = keras.utils.to_categorical(labels[1])
-  labels_test_v = labels_test[0]
-  labels_test = keras.utils.to_categorical(labels_test[1])
+  # print(keras.utils.to_categorical(labels[0]))
+  # print(keras.utils.to_categorical(labels[1]))
+  labels_v = keras.utils.to_categorical(labels[0],num_classes=6)
+  labels = keras.utils.to_categorical(labels[1],num_classes=4)
+  print(labels_v)
+  print(labels)
+  labels_test = keras.utils.to_categorical(labels_test[1],num_classes=4)
 
-  return data,data_test,labels,labels_test
+
+
+  return data,data_test,labels,labels_test,labels_v
 
 class GAN():
-  def __init__(self,frame=50,dim=50,num_class=4):
+  def __init__(self,frame=50,dim=100,num_class=4,action_class=6):
 
-    self.latent_dim = 128
+    self.latent_dim = 50
     self.num_class = num_class
-    self.optimizer = Adam(lr=0.0002,beta_1=0.9)
+    self.action_class = action_class
+    self.optimizer = Adam(lr=0.00001, beta_1=0.5)
+    self.optimizer2 = Adam(lr=0.00001, beta_1=0.5)
     self.input_shape = (frame,dim)
+    # self.data_shape = (22016,50,100)
+    # self.loss = myloss()
 
     self.encoder = self.build_encoder()
-    self.classifer = self.build_classifer()
-    self._min = self.build_min(self.encoder)
-    self.combined = self.build_combine(self.encoder,self.classifer,self._min)
 
+    self.id_classifer = self.build_ID_classifer()
+    self.id_classifer.compile(loss='categorical_crossentropy',optimizer=self.optimizer2,metrics=['accuracy'])
+    
+    self.action_classifer = self.build_action_classifer()
+    self.action_classifer.compile(loss='categorical_crossentropy',optimizer=self.optimizer2,metrics=['accuracy'])
 
-  def build_encoder(self):
+    self.combined = self.build_combine(self.encoder,self.id_classifer,self.action_classifer)
+    self.combined.compile(loss=['categorical_crossentropy',self.myloss],optimizer=self.optimizer,loss_weights=[1, 0.01],metrics=['accuracy'])
+    self.combined.summary()
+
+  def build_encoder(self,):
   
     model = Sequential()
-    model.add(Conv1D(32,kernel_size=5,padding='same',activation='relu',input_shape=self.input_shape))
-    model.add(Conv1D(32,kernel_size=5,padding='same',activation='relu'))
+
+    model.add(Conv1D(32,kernel_size=7,padding='same',activation='relu'))
+    # model.add(Conv1D(64,kernel_size=7,padding='same',activation='relu'))
     model.add(MaxPooling1D(pool_size=2))
     model.add(BatchNormalization())
 
     model.add(Conv1D(64,kernel_size=5,padding='same',activation='relu'))
-    model.add(Conv1D(64,kernel_size=5,padding='same',activation='relu'))
+    # model.add(Conv1D(128,kernel_size=5,padding='same',activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(BatchNormalization())
+    
+    model.add(Conv1D(128,kernel_size=5,padding='same',activation='relu'))
+    # model.add(Conv1D(128,kernel_size=5,padding='same',activation='relu'))
     model.add(MaxPooling1D(pool_size=2))
     model.add(BatchNormalization())
 
-    model.add(Flatten())
-    model.add(Dropout(0.5))
+    model.add(GlobalAveragePooling1D())
+    # model.add(Dropout(rate=0.3))
     model.add(Dense(self.latent_dim))
 
     return model
 
-  def build_classifer(self):
+  def build_action_classifer(self,):
 
     model = Sequential()
+        
+    model.add(Dense(64,input_dim=self.latent_dim))
+    model.add(LeakyReLU(alpha=0.2))
+    # model.add(Dropout(rate=0.3))
 
-    model.add(Dense(256,input_dim=self.latent_dim))
-    model.add(Activation('relu'))
-    
     model.add(Dense(64))
-    model.add(Activation('relu'))
+    model.add(LeakyReLU(alpha=0.2))
+    
+    model.add(Dense(128))
+    model.add(LeakyReLU(alpha=0.2))
 
-    model.add(Dense(self.num_class,activation='softmax'))
-    model.compile(loss='categorical_crossentropy',optimizer=self.optimizer,loss_weights=[0.5],metrics=['accuracy'])
+    # model.add(Dropout(rate=0.5))
 
-    return model
-  
-  def build_min(self,E):
+    model.add(Dense(128))
+    model.add(LeakyReLU(alpha=0.2))
 
-    def abs_min(pair):
-      f1,f2 = pair
-      sub = keras.layers.Subtract()([feature_1,feature_2])
-      return keras.backend.abs(sub)
+    model.add(Dense(256))
+    model.add(LeakyReLU(alpha=0.2))
 
-    input1 = Input(shape=self.input_shape)
-    input2 = Input(shape=self.input_shape)
-    feature_1 = E(input1)
-    feature_2 = E(input2)
-    out = Lambda(abs_min)([feature_1,feature_2])
+    model.add(Dense(64))
+    model.add(LeakyReLU(alpha=0.2))
 
-    model = Model([input1,input2],[out])
+    model.add(Dropout(rate=0.5))
+
+    model.add(Dense(self.action_class, activation='softmax'))
 
     return model
 
-  def build_combine(self,E,C,_min):
+  def build_ID_classifer(self,):
 
-    E.trainalbe = True
-    C.trainalbe = False
+    model = Sequential()
+        
+    model.add(Dense(64,input_dim=self.latent_dim))
+    model.add(LeakyReLU(alpha=0.2))
+    # model.add(Dropout(rate=0.3))
+
+    model.add(Dense(64))
+    model.add(LeakyReLU(alpha=0.2))
+    
+    model.add(Dense(128))
+    model.add(LeakyReLU(alpha=0.2))
+
+    # model.add(Dropout(rate=0.5))
+
+    model.add(Dense(128))
+    model.add(LeakyReLU(alpha=0.2))
+
+    model.add(Dense(256))
+    model.add(LeakyReLU(alpha=0.2))
+
+    model.add(Dense(64))
+    model.add(LeakyReLU(alpha=0.2))
+
+    model.add(Dropout(rate=0.5))
+
+    model.add(Dense(self.num_class, activation='softmax'))
+
+    return model
+
+  def build_combine(self,E,id_C,action_c):
+
+    E.trainable = True
+    id_C.trainable = False
+    action_c.trainable = False
 
     input0 = Input(shape=self.input_shape)
-    input1 = Input(shape=self.input_shape)
-    input2 = Input(shape=self.input_shape)
-    
-    feature_c = E(input0)
-    
-    C_out = C(feature_c)
-    # print(labels.shape)
 
-    min_out = _min([input1,input2])
+    feature = E(input0)
 
-    model = Model([input0,input1,input2],[C_out,min_out])
+    id_out = id_C(feature)
+    action_out = action_c(feature)
+
+
+    model = Model([input0],[id_out,action_out])
     
-    # loss1 = keras.losses.categorical_crossentropy(labels,C_out)
-    # loss2 = keras.losses.mse(feature_1,feature_2)
-    # my_loss = 0.75*loss1 + 0.25*loss2
-
-    # model.add_loss(my_loss)
-    model.compile(loss=['categorical_crossentropy','mse'],optimizer=self.optimizer,loss_weights=[3,1],metrics=['accuracy'])
     return model
 
-  # def my_loss():
-    
-  def random_select(self,data,labels,batch_size):
+  def myloss(self,y_true,y_pred):
+    return tf.negative(K.categorical_crossentropy(y_true,y_pred))
+
+  def random_select(self,data,labels,labels_v,batch_size):
     
     rand_index = np.random.randint(low=0,high=data.shape[0],size=batch_size)
     rand_data0 = data[rand_index,:,:]
     rand_labels = labels[rand_index,:]
+    rand_labels_v = labels_v[rand_index,:]
 
-    rand_index = np.random.randint(low=0,high=data.shape[0],size=batch_size)
-    rand_data1 = data[rand_index,:,:]
+    # print(labels_v.shape)
+    # print(labels.shape)
 
-    rand_index = np.random.randint(low=0,high=data.shape[0],size=batch_size)
-    rand_data2 = data[rand_index,:,:]
+    return rand_data0,rand_labels,rand_labels_v
 
-    # print(rand_data0)
-    return rand_data0,rand_labels,rand_data1,rand_data2
+  def predict_ID(self,x_test,y_test):
 
-  def my_predict(x_test,y_test):
-
-    y_test = keras.utils.to_categorical(y_test)
     feature = self.encoder.predict(x_test)
-    a = self.classifer.evaluate(feature,y_test)
-    print(a)
+    # print(y_test)
+    score = self.id_classifer.evaluate(feature,y_test,verbose=1)
+    test_loss = score[0]
+    predictions = self.id_classifer.predict(feature)
+    test_accuracy = sum([np.argmax(y_test[i])==np.argmax(predictions[i]) for i in range(len(y_test))])/len(y_test)
 
-  def train(self,data,labels,data_test,labels_test,batch_size,n_batch):
+    predictions = self.id_classifer.predict_classes(feature)
+    y_test = np.argmax(y_test,axis=1)
 
+    print(pd.crosstab(y_test,predictions,rownames=['label'],colnames=['predict'],normalize='index'))
+    print("Loss : %s \nAccuracy : %s" % (test_loss,test_accuracy))
+
+  def train(self,data,labels,data_test,labels_test,labels_v,batch_size,n_batch):
+
+    start_time = datetime.datetime.now()
     zero = np.zeros((batch_size,self.latent_dim))
     for batch in range(n_batch):
       # rand_data,rand_labels,rand_data1,rand_data2 = self.random_select(data,labels,batch_size)
 
-      # feature = self.encoder.predict(rand_data)
-      # c_loss = self.classifer.train_on_batch(feature,rand_labels)
+      rand_data,rand_labels,rand_labels_v= self.random_select(data,labels,labels_v,batch_size)
+      # rand_labels_v = rand_labels_v.reshape((128,3))
+      # rand_labels_v = np.argmax(rand_labels_v)
+      loss = self.combined.train_on_batch([rand_data],[rand_labels,rand_labels_v])
 
-      rand_data,rand_labels,rand_data1,rand_data2 = self.random_select(data,labels,batch_size)
-      # print(rand_labels)
-      # rand_labels = keras.utils.to_categorical(rand_labels)
-      # print(zero)
-      loss = self.combined.train_on_batch([rand_data,rand_data1,rand_data2],[rand_labels,zero])
-
-      if (batch+1)%100==0:
-        print('batch: %d,loss: %f]'%(batch,loss))
-        self.my_predict(data_test,labels_test)
+      if (batch+1)%1000==0:
+        elapsed_time = datetime.datetime.now() - start_time
+        print(self.combined.metrics_names)
+        print('batch: %d,loss: %s Time: %s'%(batch+1,loss,elapsed_time))
+        self.predict_ID(data_test,labels_test)
 
 if __name__ == '__main__':
-  x,x_test,y,y_test = get_data()
+  x,x_test,y,y_test,y_v = get_data()
   # print(a.shape,c.shape)
 
   distangle = GAN()
-  distangle.train(x,y,x_test,y_test,batch_size=128,n_batch=2000)
+  distangle.train(x,y,x_test,y_test,y_v,batch_size=128,n_batch=40000)
